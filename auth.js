1b17e1c26e866f7fa192dec53f7c25d574012953
Mr WTF <125758201+Omarjourney@users.noreply.github.com>
2025-06-27 12:23:42 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,25 @@
+// auth.js
 
+import { auth } from './firebase.js';
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("loginForm");
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          alert(`✅ Welcome ${user.email}`);
+        })
+        .catch((error) => {
+          alert(`❌ ${error.message}`);
+        });
+    });
+  }
+});
