528d79c5b6ccf6b2b6c39f3c10d4832b832903ec
Mr WTF <125758201+Omarjourney@users.noreply.github.com>
2025-07-25 09:21:30 -0700

    add react typings and vite

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+  <React.StrictMode>
+    <div>Feelynx</div>
+  </React.StrictMode>
+);
