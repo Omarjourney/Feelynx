37398b1c45cf228df0790ab07c5ccd063f5eccc8
Mr WTF <125758201+Omarjourney@users.noreply.github.com>
2025-07-30 22:19:36 -0700

    Serve static public assets

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Mock list of creators. Replace with a database in production.
 let creators = [
